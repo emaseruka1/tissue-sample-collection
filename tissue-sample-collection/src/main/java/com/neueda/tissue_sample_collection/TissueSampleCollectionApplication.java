de2fcd600c846a0e8fc42bd911bdbd244624d497
emaseruka1 <emmamaseruka97@gmail.com>
2025-08-16 15:55:29 +0200

    Add Spring Boot starter project files with merged gitignore and gitattributes

---
+++ tissue-sample-collection/src/main/java/com/neueda/tissue_sample_collection/TissueSampleCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.tissue_sample_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TissueSampleCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TissueSampleCollectionApplication.class, args);
+	}
+
+}
---
+++ tissue-sample-collection/src/test/java/com/neueda/tissue_sample_collection/TissueSampleCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.tissue_sample_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TissueSampleCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
